f7f386eb594bec8b3f843d0c07128014d8a4a08f
Goat <dpajbk@gmail.com>
2022-12-06 21:43:20 -0500

    server file updated

--- server/server.js
+++ server/server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const routes = require('./routes');
+const { ApolloServer} = require("apollo-server-express");
+const {typeDefs, resolvers} = require("./schemas");
+const {authMiddleware} = require("./utils/auth");
+
+// const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = new ApolloServer ({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,6 +22,10 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 app.use(routes);
 
