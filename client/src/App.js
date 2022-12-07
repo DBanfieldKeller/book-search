089634baf7adfba417b4072dd89ab5776e040400
Goat <dpajbk@gmail.com>
2022-12-06 23:01:48 -0500

    added mutations.js, started working on app.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SearchBooks from './pages/SearchBooks';
 import SavedBooks from './pages/SavedBooks';
 import Navbar from './components/Navbar';
+import { ApolloClient, ApolloProvider  } from '@apollo/client';
+
+const client = new ApolloClient({
+  request: (operation) => {
+    const token = localStorage.getItem("id_token");
+
+    operation.setContext({
+      headers: {
+        authorization: token ? `Bearer ${token}` : "",
+      },
+    });
+  },
+  uri: "/graphql",
+});
+
 
 function App() {
   return (
