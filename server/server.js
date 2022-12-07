c7a07b8d60d56b09b6a3792ac23658f8efe6c37a
Goat <dpajbk@gmail.com>
2022-12-06 22:04:44 -0500

    added resolvers and index in schemas

--- server/server.js
+++ server/server.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-const { ApolloServer} = require("apollo-server-express");
-const {typeDefs, resolvers} = require("./schemas");
-const {authMiddleware} = require("./utils/auth");
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schemas");
+const { authMiddleware } = require("./utils/auth");
 
 // const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-const server = new ApolloServer ({
+const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
@@ -29,6 +29,9 @@ app.get("/", (req, res) => {
 
 app.use(routes);
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
